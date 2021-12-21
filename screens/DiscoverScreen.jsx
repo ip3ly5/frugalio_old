4f64d9c1bf640fa2307e6acbadc88725b5f0e852
Lewis BS <ip3ly5@gmail.com>
2021-12-21 03:24:58 +0100

    initial client commit message

---
+++ screens/DiscoverScreen.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function DiscoverScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Discover!</Text>
+    </View>
+  );
+}
+
+export default DiscoverScreen;

ab4c882e7f22d38eda212aae551da4de5f8948fc
Lewis BS <ip3ly5@gmail.com>
2022-01-09 13:19:06 +0100

    gcloud setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,46 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const tesseract = require("node-tesseract-ocr")
+// Imports the Google Cloud client library.
+const {Storage} = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+// Makes an authenticated API request.
+async function listBuckets() {
+  try {
+    const results = await storage.getBuckets();
+
+    const [buckets] = results;
+
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+      console.log(bucket.name);
+    });
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+listBuckets();
+
+
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./public/banner.PNG');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+quickstart();
+
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
@@ -14,19 +53,3 @@ const receipts = require('./routes/api/receipt')
 
 app.use('/api/receipts', receipts)
 
-const img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.heritagechristiancollege.com%2Fwp-content%2Fuploads%2F2019%2F05%2Ffree-walmart-receipt-template-of-need-walmart-receipt-template-of-free-walmart-receipt-template.jpg&f=1&nofb=1"
-
-const config = {
-  lang: "eng",
-  oem: 1,
-  psm: 3
-}
-
-tesseract
-  .recognize(img, config)
-  .then((text) => {
-    console.log("Result:", text)
-  })
-  .catch((error) => {
-    console.log(error.message)
-  })
